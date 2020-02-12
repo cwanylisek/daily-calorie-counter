1c308dfd0e14f2e97374492826def504058d5a59
cwanylisek <rewiks@gmail.com>
2020-02-12 16:53:42 +0100

    select implementation

--- src/components/Inputbar/Inputbar.js
+++ src/components/Inputbar/Inputbar.js
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { MealsContext } from '../../contexts/MealsContext';
+import Select from 'react-select';
+import './Inputbar.scss';
 
 const Inputbar = () => {
 
+    const { meals } = useContext(MealsContext)
+
+    const styles = {
+        control: (provided, state) => ({
+            ...provided,
+            background: '#000',
+            border: '1px solid rgba(254, 145, 102, 0.25)',
+            boxSizing: 'border-box',
+            borderRadius: '20px',
+            width: '100%',
+            height: '32px',
+            padding: '0 15px'
+        }),
+        menu: (provided) => ({
+            ...provided,
+            width: '100%',
+            zIndex: '110',
+            background: '#000'
+        })
+    }
+
     return (
-        <div>
-            inputbar
+        <div className="inputbar__container">
+            <Select
+                options={meals}
+                placeholder={'Type your meal'}
+                isSearchable={true}
+                styles={styles}
+                theme={theme => ({
+                    ...theme,
+                    borderRadius: 0,
+                    colors: {
+                        ...theme.colors,
+                        primary25: '#FE9166',
+                        primary: '#FE9166',
+                    },
+                })}
+            />
         </div>
     )
 };
--- src/contexts/MealsContext.js
+++ src/contexts/MealsContext.js
@@ -5,7 +5,9 @@ export const MealsContext = createContext();
 
 const MealsContextProvider = props => {
     const [meals, setMeals] = useState([
-        { id: uuid(), meal: 'rice', kcal: 351, portion: 1}
+        { id: uuid(), value: 'rice', label: 'Rice', kcal: 351, portion: 100},
+        { id: uuid(), value: 'chicken breast', label: 'Chicken breast', kcal: 164, portion: 100},
+        { id: uuid(), value: 'beef', label: 'Beef', kcal: 250, portion: 100}
     ])
 
     return (
