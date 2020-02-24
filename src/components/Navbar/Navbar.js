e7cc28cf0e997bd6c8bb915d290321d5c364c7c8
cwanylisek <rewiks@gmail.com>
2020-02-24 16:08:02 +0100

    button ternary

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChartLine } from '@fortawesome/free-solid-svg-icons';
+import { faChartLine,  faUtensils} from '@fortawesome/free-solid-svg-icons';
 import './Navbar.scss';
 
 const Navbar = () => {
 
+    const location = useLocation();
+
+    let navLink
+
+    location.pathname === '/'
+        ? navLink =  <Link to="/chart"><FontAwesomeIcon className="Navbar__icon" icon={faChartLine} size="2x" /></Link>
+        : navLink = <Link to="/"><FontAwesomeIcon className="Navbar__icon" icon={faUtensils} size="2x" /></Link>
+
     return (
         <nav className="Navbar__container">
-            <Link to="/chart"><FontAwesomeIcon className="Navbar__icon" icon={faChartLine} size="2x" /></Link>
+            {navLink}
         </nav>
     )
 
