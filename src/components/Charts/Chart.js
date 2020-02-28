4ec880b17c2472208038473b2e75040820b9153d
cwanylisek <rewiks@gmail.com>
2020-02-28 18:48:54 +0100

    chart.js install && methods for charts data exposure

--- src/components/Charts/Chart.js
+++ src/components/Charts/Chart.js
@@ -1,14 +1,43 @@
 import React, { useContext } from 'react';
 import { ChartContext } from '../../contexts/ChartContext';
+import { Line } from 'react-chartjs-2';
 import './Chart.scss';
 
 const Chart = () => {
 
-    const { mealsHistory } = useContext(ChartContext);
+    const { data, labels } = useContext(ChartContext);
+
+    const chartOptions = {
+        labels: labels,
+        datasets: [
+          {
+            label: 'My First dataset',
+            fill: false,
+            lineTension: 0.8,
+            backgroundColor: 'rgb(25, 159, 132)',
+            borderColor: 'rgb(25, 159, 132)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: data
+          }
+        ]
+      };
+      
 
     return (
         <div className="chart__container">
-            <p>test</p>
+            <Line data={chartOptions}></Line>
         </div>
     )
 
--- src/contexts/ChartContext.js
+++ src/contexts/ChartContext.js
@@ -3,22 +3,37 @@ import React, { createContext, useState, useEffect } from 'react';
 export const ChartContext = createContext();
 
 const ChartContextProvider = props => {
-    const [mealsHistory, setMealsHistory] = useState([]);
+
+    const [data, setData] = useState();
+    const [labels, setLabels] = useState();
 
     useEffect(() => {
 
         //load data from LS
 
-        const serializedState = JSON.parse(localStorage.getItem('meals'));
-        console.log(serializedState, 'serizalized state')
-        const neededData = serializedState.map(item => [ item.date, item.kcal]);
-        console.log(neededData, 'nd')
-        setMealsHistory(serializedState)
-        
+        const loadDataFromLs = JSON.parse(localStorage.getItem('meals'));
+        const neededValues = loadDataFromLs.map(({ date, kcal }) => ({ date, kcal }));
+        console.log(neededValues)
+
+        const result = Object.values(neededValues.reduce((val, { kcal, date }) => {
+            if (!val[date])
+                val[date] = Object.assign({}, { kcal, date });
+            else
+                val[date].kcal += kcal;
+            return val;
+        }, {}));
+
+        // destructuring for two separate arrays
+        const data = result.map(({ kcal }) => kcal);
+        const labels = result.map(({ date }) => date);
+
+        setData(data);
+        setLabels(labels)
+
     }, []);
 
     return (
-        <ChartContext.Provider value={{ mealsHistory }}>
+        <ChartContext.Provider value={{ data, labels }}>
             {props.children}
         </ChartContext.Provider>
     )
