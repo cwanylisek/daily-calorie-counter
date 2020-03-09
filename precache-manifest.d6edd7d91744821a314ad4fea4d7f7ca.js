0bcd547b506e5122f5b5c665b0b0a11087cf2f21
root <rewiks@gmail.com>
2020-03-09 19:53:03 +0100

    Updates

---
+++ precache-manifest.d6edd7d91744821a314ad4fea4d7f7ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbb86bb78f0882c4e9746f75161b2774",
+    "url": "/daily-calorie-counter/index.html"
+  },
+  {
+    "revision": "372a41d2c4e2d018ecd9",
+    "url": "/daily-calorie-counter/static/css/main.6f40737c.chunk.css"
+  },
+  {
+    "revision": "ba57c8979c7b86b8db4a",
+    "url": "/daily-calorie-counter/static/js/2.366ec011.chunk.js"
+  },
+  {
+    "revision": "e0ad3387081147deea4b08b0e0fff77f",
+    "url": "/daily-calorie-counter/static/js/2.366ec011.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "372a41d2c4e2d018ecd9",
+    "url": "/daily-calorie-counter/static/js/main.5fbd4158.chunk.js"
+  },
+  {
+    "revision": "aa8a9234fd95ecab5479",
+    "url": "/daily-calorie-counter/static/js/runtime-main.e8b980bd.js"
+  }
+]);
